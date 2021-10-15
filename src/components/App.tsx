f27a8827aac0fcda351d46b5ced3c0f43714bf4f
Jayd Page <jayd.page@chillisoft.co.za>
2021-10-15 14:54:06 +0200

    Add area component

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import Area from './Area'
+
+function App() {
+  return (
+    <div className="App">
+      <Area />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Area.tsx
@@ -0,0 +1,5 @@
+function Area() {
+  return <div className="Area">Area weather details!</div>
+}
+
+export default Area
