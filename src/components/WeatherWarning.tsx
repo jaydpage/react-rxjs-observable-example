ac30557db80e6fc90455b2c859dd33c72364f9cb
Jayd Page <jayd.page@chillisoft.co.za>
2021-11-26 15:47:55 +0200

    Update how weather warnings are displayed

--- src/components/WeatherWarning.tsx
+++ src/components/WeatherWarning.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { WeatherWarningProps } from './WeatherWarningContainer'
+import './WeatherWarning.css'
 
 export const WeatherWarning: React.FunctionComponent<WeatherWarningProps> = (
   props,
 ) => {
-  if (!props.loading && !props.loaded) {
+  useEffect(() => {
     props.loadWeatherWarnings()
-  }
-  return <div>{JSON.stringify(props)}</div>
+  })
+
+  return (
+    <div>
+      {props.warnings.map((warning) => (
+        <div className="weather-warning">
+          <h3>Effective</h3>
+          <div>{warning.effective}</div>
+          <h3>Description</h3>
+          <div>{warning.description}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
